e8fa2e48bae3e4f88efe2e310dc2611b62f1abd4
liyaSileshi <liyasileshi@gmail.com>
2021-02-22 21:58:26 -0800

    done

---
+++ remove_control_flag.py
@@ -0,0 +1,13 @@
+# Remove control flag
+def find_food(food, fridge):
+    for item in fridge:
+        if food in item:
+            item_name = item
+            return item_name
+    return None
+
+if __name__ == "__main__":
+    food = 'wesabi'
+    food_list = ['onion', 'cucumber','Guacamole', 'kabob barg', 'wesabi']
+    found_item = find_food(food, food_list)
+    print(food, "Found: " + found_item  if found_item != None else "not found")
